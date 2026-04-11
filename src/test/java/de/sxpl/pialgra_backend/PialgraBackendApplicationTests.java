3e52afd3eb024560ea8041eb9a2898e4af16fc89
sxpl-DavidSchmidt <damidtyt@gmail.com>
2026-04-11 13:43:34 +0200

    Initial commit

---
+++ src/test/java/de/sxpl/pialgra_backend/PialgraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sxpl.pialgra_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PialgraBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
