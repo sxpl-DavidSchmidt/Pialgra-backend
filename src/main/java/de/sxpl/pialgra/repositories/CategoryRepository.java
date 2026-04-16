1e845686acc5380ef07e0c0dd2615ef6b20ea061
sxpl-DavidSchmidt <damidtyt@gmail.com>
2026-04-16 16:25:01 +0200

    Remove unused import

--- src/main/java/de/sxpl/pialgra/repositories/CategoryRepository.java
+++ src/main/java/de/sxpl/pialgra/repositories/CategoryRepository.java
@@ -5,7 +5,6 @@ import de.sxpl.pialgra.entities.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.UUID;
 
 @Repository
