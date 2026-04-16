cd1be0f77dc391c5c795ea58d2e9335b6f8f5cbc
sxpl-DavidSchmidt <damidtyt@gmail.com>
2026-04-16 16:20:54 +0200

    Added methods to repositories

--- src/main/java/de/sxpl/pialgra/repositories/CategoryRepository.java
+++ src/main/java/de/sxpl/pialgra/repositories/CategoryRepository.java
@@ -1,10 +1,14 @@
 package de.sxpl.pialgra.repositories;
 
 import de.sxpl.pialgra.entities.CategoryEntity;
+import de.sxpl.pialgra.entities.UserEntity;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
-public interface CategoryRepository extends CrudRepository<CategoryEntity, UUID> {}
+public interface CategoryRepository extends CrudRepository<CategoryEntity, UUID> {
+    Iterable<CategoryEntity> getByUser(UserEntity user);
+}
--- src/main/java/de/sxpl/pialgra/repositories/UserRepository.java
+++ src/main/java/de/sxpl/pialgra/repositories/UserRepository.java
@@ -7,5 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface UserRepository extends CrudRepository<UserEntity, UUID> {}
+public interface UserRepository extends CrudRepository<UserEntity, UUID> {
+    UserEntity getByUsername(String username);
+}
 
