a74d8e57ac0ca3fbefda87ee12685773ed27cf38
sxpl-DavidSchmidt <damidtyt@gmail.com>
2026-04-16 17:00:54 +0200

    Remove unused import

--- src/main/java/de/sxpl/pialgra/controllers/UserController.java
+++ src/main/java/de/sxpl/pialgra/controllers/UserController.java
@@ -4,7 +4,6 @@ import de.sxpl.pialgra.domain.dtos.UserDto;
 import de.sxpl.pialgra.domain.entities.UserEntity;
 import de.sxpl.pialgra.mappers.Mapper;
 import de.sxpl.pialgra.service.UserService;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
