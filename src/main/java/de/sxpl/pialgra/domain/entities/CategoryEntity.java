881997d8e4e4c8cec383038e9ce7fc53ffc96c8b
sxpl-DavidSchmidt <damidtyt@gmail.com>
2026-04-17 13:10:59 +0200

    Configure UUID generation for `CategoryEntity` and enable `create-drop` for JPA in test configuration

--- src/main/java/de/sxpl/pialgra/domain/entities/CategoryEntity.java
+++ src/main/java/de/sxpl/pialgra/domain/entities/CategoryEntity.java
@@ -14,6 +14,7 @@ import java.util.UUID;
 @Table(name = "categories")
 public class CategoryEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID uuid;
 
     @ManyToOne(cascade = CascadeType.ALL)
