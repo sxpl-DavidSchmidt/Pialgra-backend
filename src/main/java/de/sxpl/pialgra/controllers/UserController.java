8b820b4822444bc2978b44d06e6074f01f8ec63d
sxpl-DavidSchmidt <damidtyt@gmail.com>
2026-04-20 17:52:15 +0200

    Refactor `UserController`: Simplify endpoints, standardize response structure with `ResponseEntity`, and adjust request mappings

--- src/main/java/de/sxpl/pialgra/controllers/UserController.java
+++ src/main/java/de/sxpl/pialgra/controllers/UserController.java
@@ -4,9 +4,14 @@ import de.sxpl.pialgra.domain.dtos.UserDto;
 import de.sxpl.pialgra.domain.entities.UserEntity;
 import de.sxpl.pialgra.mappers.Mapper;
 import de.sxpl.pialgra.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(path = "/api/v1/users")
 public class UserController {
     private final UserService userService;
     private final Mapper<UserEntity, UserDto> userMapper;
@@ -16,26 +21,32 @@ public class UserController {
         this.userMapper = userMapper;
     }
 
-    @GetMapping(path = "/users")
-    public Iterable<UserDto> getUsers(
+    @GetMapping
+    public ResponseEntity<List<UserDto>> getUsers(
             @RequestParam(required = false) String username
     ) {
+        List<UserDto> users;
         if (username != null) {
-            return userService.getUsersByUsername(username)
+            users = userService.getUsersByUsername(username)
+                    .stream()
+                    .map(userMapper::mapTo)
+                    .toList();
+        } else {
+            users = userService.getUsers()
                     .stream()
                     .map(userMapper::mapTo)
                     .toList();
         }
-        return userService.getUsers()
-                .stream()
-                .map(userMapper::mapTo)
-                .toList();
+        return ResponseEntity.ok(users);
     }
 
-    @PostMapping(path = "/users/create")
-    public UserDto createUser(@RequestBody UserDto user) {
+    @PostMapping(path = "/create")
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto user) {
         UserEntity userEntity = userMapper.mapFrom(user);
         UserEntity savedUserEntity = userService.createUser(userEntity);
-        return userMapper.mapTo(savedUserEntity);
+        return new ResponseEntity<>(
+                userMapper.mapTo(savedUserEntity),
+                HttpStatus.CREATED
+        );
     }
 }
