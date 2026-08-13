78fccf358914a028d623828642e9f56dab27e752
sxpl-DavidSchmidt <damidtyt@gmail.com>
2026-08-13 12:33:43 +0200

    Temporarily add automatic category creation logic

--- src/main/java/de/sxpl/pialgra/service/impl/AuthServiceImpl.java
+++ src/main/java/de/sxpl/pialgra/service/impl/AuthServiceImpl.java
@@ -1,8 +1,10 @@
 package de.sxpl.pialgra.service.impl;
 
+import de.sxpl.pialgra.domain.entities.CategoryEntity;
 import de.sxpl.pialgra.domain.entities.UserEntity;
 import de.sxpl.pialgra.exceptions.UsernameAlreadyExistsException;
 import de.sxpl.pialgra.service.AuthService;
+import de.sxpl.pialgra.service.CategoryService;
 import de.sxpl.pialgra.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -10,18 +12,30 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
     private final UserService userService;
     private final AuthenticationManager authenticationManager;
+    private final CategoryService categoryService;
 
     @Override
     public UserEntity register(UserEntity user) {
         if (userService.existsByUsername(user.getUsername())) {
             throw new UsernameAlreadyExistsException("Username is already taken.");
         }
-        return userService.createUser(user);
+        UserEntity createdUser = userService.createUser(user);
+
+        for (String categoryName: List.of("Algebra", "Geometry", "Psychology", "Programming")) {
+            CategoryEntity category = new CategoryEntity();
+            category.setName(categoryName);
+            category.setUser(createdUser);
+            categoryService.createCategory(category, user.getUsername());
+        }
+
+        return createdUser;
     }
 
     @Override
