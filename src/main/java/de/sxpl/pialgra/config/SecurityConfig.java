6b9b47dfe5ae8e70330d0fe55aeb7d770eb97b50
sxpl-DavidSchmidt <damidtyt@gmail.com>
2026-08-13 12:33:18 +0200

    Add containerization

--- src/main/java/de/sxpl/pialgra/config/SecurityConfig.java
+++ src/main/java/de/sxpl/pialgra/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of(
                 "http://localhost:5173",
-                "http://127.0.0.1:5173"
+                "http://localhost:8081"
         ));
 
         config.setAllowedMethods(List.of(
