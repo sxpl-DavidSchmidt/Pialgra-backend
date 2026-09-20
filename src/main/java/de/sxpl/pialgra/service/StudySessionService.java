e3abaf481e96384b65822693786f062aef227fd8
sxpl-DavidSchmidt <damidtyt@gmail.com>
2026-09-20 22:43:19 +0200

    Remove `@Service` annotations from service interfaces

--- src/main/java/de/sxpl/pialgra/service/StudySessionService.java
+++ src/main/java/de/sxpl/pialgra/service/StudySessionService.java
@@ -1,11 +1,9 @@
 package de.sxpl.pialgra.service;
 
 import de.sxpl.pialgra.domain.entities.StudySessionEntity;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface StudySessionService {
     StudySessionEntity updateStudySession(java.util.UUID uuid, de.sxpl.pialgra.domain.dtos.studysession.UpdateStudySessionDto changes, String username);
     void deleteStudySession(java.util.UUID uuid, String username);
--- src/main/java/de/sxpl/pialgra/service/UserService.java
+++ src/main/java/de/sxpl/pialgra/service/UserService.java
@@ -2,12 +2,10 @@ package de.sxpl.pialgra.service;
 
 import de.sxpl.pialgra.domain.entities.ImageEntity;
 import de.sxpl.pialgra.domain.entities.UserEntity;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
-@Service
 public interface UserService {
     List<UserEntity> findAll();
     Optional<UserEntity> findByUsername(String username);
